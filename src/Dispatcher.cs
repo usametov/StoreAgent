4d7b59eba588518792717bb06f1d039da25b796e
Alan Samet <kevin.krebson@gmail.com>
2024-11-02 23:18:57 -0400

    feat: Implement conversation workflow and product search in Dispatcher

--- src/Dispatcher.cs
+++ src/Dispatcher.cs
@@ -4,15 +4,18 @@ using System.Diagnostics;
 using Serilog;
 using StoreAgent.Helpers;
 using StoreAgent.Services;
+using StoreAgent.Models;
 
 namespace StoreAgent;
+
 public class Dispatcher {
     private IAIService? aiService;
     private IProductService? productService;
+    private ConversationStateMachine workflow = new ConversationStateMachine();  
 
     private IShoppingCart? shoppingCart;
 
-    public Dispatcher() {        
+    public Dispatcher() {              
     }
 
     public void Init() {
@@ -66,11 +69,88 @@ public class Dispatcher {
         //Log.Information($"inflated {this.productService?.GetSimilarProducts("", "")?.Count} products");        
         Log.Information($"Department names: {String.Join(",", this.productService?.GetDepartmentNames() ?? new string[]{})}");
     }
-    //TODO: add method to start conversation
-    public void StartConversation()
+
+    public string ListenToCustomer() {
+
+        var inquire = Console.ReadLine();
+        while(string.IsNullOrEmpty(inquire)) {
+            inquire = Console.ReadLine();
+        }
+        return inquire;
+    }
+
+    public void DisplaySearchResults(List<ProductSearchResult> searchResult) {
+
+        Console.Write(CommonUtils.StringifyProductSearchResult(searchResult));        
+        Console.WriteLine("Please review product search result and enter list of product SKUs and quantities, separated by colon. E.g. SKU1:2,SKU2:4 ");                   
+        Console.WriteLine("If you are not satisfied with the search result, then feel free to search again.");
+    }
+
+    public ConversationStateMachine SetupSearch(ConversationStateMachine workflow,
+                                                ConversationIntent intent) 
     {
-        var response = string.Join(", ", this.productService?.GetDepartmentNames() ?? new string[]{});
-        //TODO: add greetings and list store departments.
-        //Console.WriteLine(response);
+        workflow.QueryEmbedding =
+                    this.aiService?.GenerateEmbedding(intent.ProductDescription);
+                
+        workflow.Department = intent.Department;
+        workflow.ProductService = productService;                
+        return workflow;
+    }
+
+    
+    public void StartConversation()
+    {        
+        //listen to customer
+        var inquiry = ListenToCustomer();
+        // try to understand what he wants    
+        var aiResponse = this.aiService?.ExtractIntent(inquiry);
+
+        
+        workflow.Engage();
+
+        while(aiResponse?.FreeText!=PromptHelper.TERMINATE) {
+
+            if(aiResponse?.FreeText == PromptHelper.ABORT) {
+                Console.WriteLine("Sorry, I can't help you here. Do you want to search again?");
+                aiResponse = this.aiService?.ExtractIntent(ListenToCustomer());
+                continue;
+            }                
+
+            if(CommonUtils.IsValid(aiResponse?.ConversationIntent)) {                                
+                
+                workflow = SetupSearch(workflow, aiResponse.ConversationIntent);
+                workflow.SearchProduct();                   
+                //if product search is empty, say, we do not have the product requested.                
+                if(workflow.ProductSearchResults?.Count() == 0) {
+                    Console.WriteLine("Sorry, we could not find any product matching your inquiry. Do you want to search again?");
+                } else {                    
+                    //display product search result                    
+                    DisplaySearchResults(workflow.ProductSearchResults);
+                    inquiry = ListenToCustomer();
+                    var orderItems = CommonUtils.TryParseSKUs(inquiry, workflow.ProductSearchResults);
+                    if(orderItems.Count() == 0) 
+                    {
+                        Log.Information("could not parse order items", inquiry);
+                        aiResponse = this.aiService?.ExtractIntent(inquiry);
+                        continue;
+                    }
+                    else {
+                        workflow.AddProducts(orderItems);
+                        //TODO: Thank you, please come again.
+                        //Display transaction receipt.
+                        break;
+                    }      
+                }                
+
+            } else {
+
+                Console.WriteLine("Sorry, I did not get that. Could you please repeat your query?");    
+                aiResponse = this.aiService?.ExtractIntent(ListenToCustomer());
+                continue;
+            }
+
+        }
+
+        workflow.Finish();
     }
 }

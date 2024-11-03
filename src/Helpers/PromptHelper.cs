9e56ef3e52774fb17d55d6f49701a736c43a9c35
Alan Samet <kevin.krebson@gmail.com>
2024-11-03 14:36:43 +0000

    improve prompt

--- src/Helpers/PromptHelper.cs
+++ src/Helpers/PromptHelper.cs
@@ -11,7 +11,9 @@ public class PromptHelper {
     public string[] Departments {get;set;}    
     public string FormatDepartments() {return string.Join(", ", this.Departments);}
     public string GetSystemPromptTemplate() =>         
-        $"You are working in store as sales consultant that helps people lookup products in our store. These are departments in our store: {FormatDepartments()}.  All your responses should be in json format with these fields: FreeText to hold a text and ConversationIntent which is object with the these fields - ProductDescription, Department, Quantity,  minPrice, maxPrice. Quantity is a whole number,  minPrice and maxPrice are decimal numbers and the rest of the fields are strings. Default values for minPrice and maxPrice are 0.01 and 1000.00. If the customer wants to terminate the conversation, then set FreeText to  TERMINATE. If the customer asks for something that is not relevant to what our store might have then set FreeText to ABORT.";
+        $"You are working in store as sales consultant that helps people lookup products in our store. These are departments in our store: {FormatDepartments()}. "
+        + " All your responses should be in json format with these fields: FreeText to hold a text and ConversationIntent which is object with the these fields - ProductDescription, Department, Quantity,  minPrice, maxPrice. Quantity is a whole number,  minPrice and maxPrice are decimal numbers and the rest of the fields are strings. Default values for minPrice and maxPrice are 0.01 and 1000.00. If the customer wants to terminate the conversation, then set FreeText to  TERMINATE. If the customer asks for something that is not relevant to what our store might have then set FreeText to ABORT."
+        + "If customer query looks like sequence of SKU:Quantity pairs separated by comma then set FreeText to ORDER_READY. Here is an example of sequence of SKU:Quantity pairs: SKU12:1,RTX:3,WRT:5 - here SKU is random alphanumeric sequence of 3 to 8 characters.";
 
     public PromptHelper(string[] Departments) {
         this.Departments = Departments;
